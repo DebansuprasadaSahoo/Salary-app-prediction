164641ca301daa7b83f2e3c70b6e534b7ff04c16
Debansu prasada Sahoo <debansuprasada@gmail.com>
2024-10-23 12:34:57 +0530

    Add files via upload

---
+++ salary_app_frontend.py
@@ -0,0 +1,19 @@
+import numpy as np
+import streamlit as st
+import pickle
+
+model = pickle.load(open("linear_regression_model.pkl", "rb"))
+
+st.title('Salary prediction app')
+
+st.write('This app predicts the salary with respect to the years of experience')
+
+years_experience = st.number_input("Enter years of experience :", min_value=0.0, max_value=50.0, value = 1.0, step=0.5)
+
+if st.button("Predict salary"):
+    experience_input = np.array([[years_experience]])
+    prediction = model.predict(experience_input)
+    
+    st.success(f'The predicted salary is {years_experience} years of experience is : ${prediction[0]:,.2f}')
+    
+st.write("This prediction is formed from the salary and years of experience dataset")
